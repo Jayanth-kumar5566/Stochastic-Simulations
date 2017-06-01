e9a9f6c2ba71cf26f6df5a618307f279377b3c36
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2017-06-01 07:46:26 +0530

    modified Rcode

--- Stochastic_stimulation_SIR/sigmoid_slope.R
+++ Stochastic_stimulation_SIR/sigmoid_slope.R
@@ -1,6 +1,5 @@
-set.seed(100)
-x_ <- read.csv("Stochastic-Simulations/Stochastic_stimulation_SIR/x.csv")
-y_ <- read.csv("Stochastic-Simulations/Stochastic_stimulation_SIR/y.csv")
+x_ <- read.csv("x.csv")
+y_ <- read.csv("y.csv")
 x<-x_$X0
 y<-log(y_$X0)
 plot(x,y)
@@ -11,10 +10,17 @@ plot.ts(log.NQ)
 low <- lowess(log.NQ) #statsmodels.nonparametric.smoothers_lowess
 plot(low)
 cutoff <- 0.75
-q <- quantile(low$y, cutoff)#scipy.stats.mstats.mquatiles
+q <- quantile(low$y, cutoff,na.rm=TRUE)#scipy.stats.mstats.mquatiles
 plot.ts(log.NQ)
 abline(h=q)
 x.lower <- x[min(which(low$y > q))]
 x.upper <- x[max(which(low$y > q))]
 plot(x,y)
 abline(v=c(x.lower, x.upper))
+i=which(x.upper == x)
+j=which(x.lower == x)
+y_new=y[j:i]
+x_new=x[j:i]
+plot(x_new,y_new)
+fit <- lm(y_new ~ x_new)   # y 'as a linear function of' x
+abline(fit) 
