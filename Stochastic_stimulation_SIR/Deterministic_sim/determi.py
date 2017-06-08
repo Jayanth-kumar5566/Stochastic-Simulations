f86206ab742c8df8332d5472055795dc3c138361
N.Jayanth Kumar <njayanth@haldi.imsc.res.in>
2017-06-08 20:51:46 +0530

    version 3

--- Stochastic_stimulation_SIR/Deterministic_sim/determi.py
+++ Stochastic_stimulation_SIR/Deterministic_sim/determi.py
@@ -3,27 +3,38 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
-def dydt2Cities(Y,t,beta1,beta2,gamma):
+def dydt2Cities(Y,t,beta1,beta2,gamma,tr12,tr21):
     S1,I1,R1,S2,I2,R2 = Y
-    dS1dt = -beta1*S1*I1
-    dI1dt = beta1*S1*I1 - gamma*I1
-    dR1dt = gamma*I1
-    dS2dt = -beta2*S2*I2
-    dI2dt = beta2*S2*I2 - gamma*I2
-    dR2dt = gamma*I2
+    dS1dt = -beta1*S1*I1 -beta1*S1*tr21*I2 +tr21*S2 -tr12*S1
+    dI1dt = beta1*S1*I1 - gamma*I1 +tr21*I2*beta1*S1 -gamma*I2*tr21 +tr21*I2 -tr12*I1
+    dR1dt = gamma*I1 +gamma*tr21*I2+ tr21*R2 -tr12*R1
+    dS2dt = -beta2*S2*I2 -beta2*S2*tr12*I1 +tr12*S1 -tr21*S2
+    dI2dt = beta2*S2*I2 - gamma*I2 +tr12*I1*beta2*S2 -gamma*I1*tr12 +tr12*I1 -tr21*I2
+    dR2dt = gamma*I2 +gamma*tr12*I1+ tr12*R1 -tr21*R2
     return np.array([dS1dt,dI1dt,dR1dt,dS2dt,dI2dt,dR2dt])
 
-def Simulate(tmax,beta1,beta2,gamma):
+def Simulate(tmax,beta1,beta2,gamma,tr12,tr21):
  	Y_in = np.array([1000,1,0,1000,1,0])
-	t = np.arange(0,tmax,0.001)
-	Y = odeint(dydt2Cities,Y_in,t,args=(beta1,beta2,gamma,))
-	print "Ro is ", (beta1*1000)/float(gamma)	
-	fig,ax = plt.subplots()
-	ax.plot(t,Y)
-	plt.show()
-	#return Y[:,1]
+	t = np.arange(0,tmax,0.01)
+	Y = odeint(dydt2Cities,Y_in,t,args=(beta1,beta2,gamma,tr12,tr21,))
+	#print "Ro is ", (beta1*1000)/float(gamma)	
+	#fig,ax = plt.subplots()
+	#ax.plot(t,Y,label=('S1','I1','R1','S2','I2','R2'))
+	#ax.legend(loc='best')
+	#plt.show()
+	return (t,Y)
 # if __name__ == '__main__':
-Simulate(1000,0.0002,0.0002,0.1)
+Simulate(1000,0.0002,0.0005,0.1,0.4,0.4)
+tr=np.arange(0.1,1,0.1)
+labels=['S1','I1','R1','S2','I2','R2']
+for i in tr:
+	(t,Y)=Simulate(100,0.0002,0.0005,0.1,i,i)
+	plt.clf()	
+	plt.figure(figsize=(30,15))	
+	for y ,label in zip(Y.transpose(),labels):	
+		plt.plot(t,y,label=label)
+	plt.legend(loc='best')
+	plt.savefig(str(i)+'.png', format='png', orientation='landscape')
 '''
 file=open('y.csv','w')
 for i in z:
--- Stochastic_stimulation_SIR/Final/Version_2/ans_graph.py
+++ Stochastic_stimulation_SIR/Final/Version_2/ans_graph.py
@@ -2,20 +2,21 @@ from __future__ import division
 import SIR_functions
 import numpy
 import  matplotlib.pyplot as plt
+from scipy import polyfit
 N1 = 1000
 N2 = 1000
 mu = 0
 gamma = 0.1
 omega = 0
-tr12 = 0
-tr21 = 0
+#tr12 = 0
+#tr21 = 0
 tmax = 100
 alpha = 0
 #beta1=0.0002
 #beta2=0.0003
 
 #----------------------------------Averaging of the series-----------------------------------------------------------
-def sim_av(beta1,beta2):
+def sim_av(beta1,beta2,tr12,tr21):
     series_t1=[]
     series_t2=[]
     series_total=[]
@@ -92,38 +93,37 @@ def fit(ser,tim):
 #-------------------------------------------------------------------------------------------
 #beta_values=[(0.0002,0.0003),(0.0002,0.0004),(0.0002,0.0005),(0.0002,0.0006),(0.0002,0.0007),(0.0002,0.0008),(0.0002,0.0009),(0.0002,0.0010)]
 
-x=[]
-y=[]
-z=[]
-for i in numpy.arange(0.0002,0.0010,0.00001):
-    [(ser_t1,tim_t1),(ser_t2,tim_t2),(ser_tot,tim_tot)]=sim_av(0.0002,i)
-    xx=fit(ser_t1,tim_t1)
-    yy=fit(ser_t2,tim_t2)
-    zz=fit(ser_tot,tim_tot)
-    x.append(xx[1])
-    y.append(yy[1])
-    z.append(zz[1])
+tr_val=numpy.linspace(1e-8,1e-1,20)
+count=0
+for (n,m) in zip(tr_val,tr_val):
+    x=[]
+    y=[]
+    z=[]
+    for i in numpy.arange(0.0002,0.0010,0.00001):
+        fixed_r=0.0002
+        [(ser_t1,tim_t1),(ser_t2,tim_t2),(ser_tot,tim_tot)]=sim_av(fixed_r,i,n,m)
+        xx=(fixed_r*N1)/gamma
+        yy=(i*N2)/gamma
+        zz=fit(ser_tot,tim_tot)
+        x.append(xx)
+        y.append(yy)
+        z.append(1+(zz[1]/gamma))
 
-from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, z, c='r', marker='o')
-ax.set_xlabel('R0 of city 1')
-ax.set_ylabel('R0 of city 2')
-ax.set_zlabel('total R0')
-plt.show()
-x=numpy.array(x)
-y=numpy.array(y)
-z=numpy.array(z)
-Xax=abs(x-y)
-me=(x+y)/2.0
-ma=numpy.maximum.reduce([x,y])
-mi=numpy.minimum.reduce([x,y])
-plt.plot(Xax,z,'bo',label='actual total0')
-plt.plot(Xax,me,'g*',label='mean of ro')
-plt.plot(Xax,ma,'r^',label='max of the ro')
-plt.plot(Xax,mi,'kv',label='min of the ro')
-plt.legend(loc='best')
-plt.show()
+    x=numpy.array(x)
+    y=numpy.array(y)
+    z=numpy.array(z)
+    Xax=abs(x-y)
+    me=(x+y)/2.0
+    ma=numpy.maximum.reduce([x,y])
+    mi=numpy.minimum.reduce([x,y])
 
-
+    plt.clf()
+    plt.figure(figsize=(30,15))
+    plt.plot(Xax,z,'bo',label='actual total0')
+    plt.plot(Xax,me,'g*',label='mean of ro')
+    plt.plot(Xax,ma,'r^',label='max of the ro')
+    plt.plot(Xax,mi,'kv',label='min of the ro')
+    plt.legend(loc='best')
+    plt.savefig(str(count)+'.png', format='png', orientation='landscape')
+    count +=1
+    print count
