4f8ff61daa073e6c9bbee4aa407f97aaf8c8621e
N.Jayanth Kumar <njayanth@haldi.imsc.res.in>
2017-06-12 11:00:36 +0530

    deterministic

--- Stochastic_stimulation_SIR/SIER/Determin/determi.py
+++ Stochastic_stimulation_SIR/SIER/Determin/determi.py
@@ -1,26 +1,34 @@
 from __future__ import division
 import numpy as np
 from scipy.integrate import odeint
+import matplotlib as mpl
+mpl.use("Agg")
 import matplotlib.pyplot as plt
 
 
-def dydt2Cities(Y,t,beta1,gamma,alpha):
+def dydt2Cities(Y,t,beta1,beta2,gamma,sigma,tr12,tr21):
     S1 = Y[0]
     E1 = Y[1]
     I1 = Y[2]
     R1 = Y[3]
-    N=S1+E1+I1+R1
-    print S1
-    dS1dt = -(beta1*S1*I1)/N
-    dE1dt =  (beta1*S1*I1)/N -alpha*E1 
-    dI1dt =  alpha*E1 - gamma*I1 
-    dR1dt =  gamma*I1 
-    return np.array([dS1dt,dI1dt,dR1dt])
-
-def Simulate(tmax,beta1,gamma,alpha):
- 	Y_in = np.array([10,1,1,0])
+    S2 = Y[4]
+    E2 = Y[5]
+    I2 = Y[6]
+    R2 = Y[7]
+    dS1dt = -beta1*S1*I1 -tr12*S1 +tr21*S2
+    dE1dt =  beta1*S1*I1 -sigma*E1 -tr12*E1 +tr21*E2
+    dI1dt =  sigma*E1 - gamma*I1 
+    dR1dt =  gamma*I1 -tr12*R1 +tr21*R2
+    dS2dt = -beta2*S2*I2 -tr21*S2 +tr12*S1
+    dE2dt =  beta2*S2*I2 -sigma*E2 -tr21*E2 +tr12*E1
+    dI2dt =  sigma*E2 - gamma*I2
+    dR2dt =  gamma*I2 -tr21*R2 +tr12*R1
+    return np.array([dS1dt,dE1dt,dI1dt,dR1dt,dS2dt,dE2dt,dI2dt,dR2dt])
+ 
+def Simulate(tmax,beta1,beta2,gamma,sigma,tr12,tr21):
+ 	Y_in = np.array([10,1,0,0,10,0,0,0])
 	t = np.arange(0,tmax,0.001)
-	Y = odeint(dydt2Cities,Y_in,t,args=(beta1,gamma,alpha))
+	Y = odeint(dydt2Cities,Y_in,t,args=(beta1,beta2,gamma,sigma,tr12,tr21))
 	#print "Ro is ", (beta1*1000)/float(gamma)	
         '''
         fig,ax = plt.subplots()
@@ -28,23 +36,28 @@ def Simulate(tmax,beta1,gamma,alpha):
 	plt.show()
 	'''
         return (t,Y)
-#Simulate(1000,1,1,1)    
+    
+
+
+fig,ax=plt.subplots(2,sharex=True)
+(t,Y)=Simulate(100,0.9,0.9,0.2,0.5,0.1,0.1)
+Y=Y.transpose()
+ax[0].plot(t,Y[0],label="S1")
+ax[0].plot(t,Y[1],label="E1")
+ax[0].plot(t,Y[2],label="I1")
+ax[0].plot(t,Y[3],label="R1")
+ax[1].plot(t,Y[4],label="S2")
+ax[1].plot(t,Y[5],label="E2")
+ax[1].plot(t,Y[6],label="I2")
+ax[1].plot(t,Y[7],label="R2")
+ax[1].legend(loc='best')
+ax[0].legend(loc='best')
+plt.savefig("test.pdf")
 
 
 
-labels=['S1','E1','I1','R1']
-(t,Y)=Simulate(15,0.9,0.2,0.5)
-for y ,label in zip(Y.transpose(),labels):	
-    plt.plot(t,y,label=label)
-plt.legend(loc='best')
-plt.show()
 
 
-
-
-
-# if __name__ == '__main__':
-#Simulate(1000,0.0002,0.0005,0.1,0.4,0.4)
 
 '''
 count=0
